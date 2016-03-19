1857a66cc5ad4cd2779c23f8fb312c9cb826b6eb
Thomas Kolar <thomas.kolar@email.de>
2016-03-19 20:31:26 +0100

    Adds the moethods getCurrentSlide, getNextSlide and getPreviousSlide

--- app/scripts/slidecontrol.js
+++ app/scripts/slidecontrol.js
@@ -2,9 +2,28 @@ class SlideController {
   constructor(slideCollection, container) {
     this.slideCollection = slideCollection;
     this.container = container;
+    this.currentSlideIndex = 0;
   }
 
   play() {
-    this.container.setAttribute('src', this.slideCollection.slides[0].url);
+    this.container.setAttribute('src', this.getNextSlide().url);
+  }
+
+  getCurrentSlide() {
+    return this.slideCollection.slides[this.currentSlideIndex]
+  }
+
+  getNextSlide() {
+    return
+      ++this.currentSlideIndex >= this.slideCollection.length
+      ? this.slideCollection.slides[0]
+      : this.slideCollection.slides[this.currentSlideIndex]
+  }
+
+  getPreviousSlide() {
+    return
+      --this.currentSlideIndex < 0
+      ? this.slideCollection.slides[this.slideCollection.length]
+      : this.slideCollection.slides[this.currentSlideIndex]
   }
 }
