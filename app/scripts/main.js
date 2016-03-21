f9dc43c47f1dea6b79738610aae38bb5c4dc2c17
Thomas Kolar <thomas.kolar@email.de>
2016-03-21 21:19:36 +0100

    Switches a demo url to a website with no iframe check

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 var collection = new Collection([]);
-collection.addSlide(new Slide('Familienblog', 'http://www.bmw.de', 5000));
+collection.addSlide(new Slide('Familienblog', 'http://www.opel.de', 5000));
 collection.addSlide(new Slide('Badminton App', 'http://smash.cologne', 5000));
 collection.addSlide(new Slide('Apple Webseite', 'http://apple.de', 5000));
 
