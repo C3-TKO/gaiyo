44c5647659eced2b69fafdf4910040e64e53e876
Thomas Kolar <thomas.kolar@email.de>
2016-03-25 12:30:01 +0100

    Adds handling of display of start/stop button within the controls component depending on the isPlaying prop

--- src/components/ControlsComponent.js
+++ src/components/ControlsComponent.js
@@ -5,12 +5,30 @@ import React from 'react';
 require('styles//Controls.scss');
 
 class ControlsComponent extends React.Component {
+
+  renderStopButton() {
+    if (this.props.isPlaying) {
+      return <button id="stop">Stop</button>
+    }
+
+    return <button id="stop" style={{display: 'none'}}>Stop</button>
+
+  }
+
+  renderPlayButton() {
+    if (this.props.isPlaying) {
+      return <button id="play" style={{display: 'none'}}>Play</button>
+    }
+
+    return <button id="play">Play</button>
+  }
+
   render() {
     return (
       <div className="controls-component">
           <button id="rewind">Rewind</button>
-          <button id="stop">Stop</button>
-          <button id="play">Play</button>
+          {this.renderStopButton()}
+          {this.renderPlayButton()}
           <button id="forward">Forward</button>
       </div>
     );
