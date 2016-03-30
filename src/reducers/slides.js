ccafa3a892a012f3003ed6fed3e35ae92939d7f9
Thomas Kolar <thomas.kolar@email.de>
2016-03-30 20:16:18 +0200

    Prepares the handling of the timeout handling

--- src/reducers/slides.js
+++ src/reducers/slides.js
@@ -20,28 +20,46 @@ module.exports = function(state = initialState, action) {
 
   switch(action.type) {
     case 'NEXT': {
+      clearTimeout(nextState.timeout);
       if (++nextState.pointer >= nextState.collection.length) {
         nextState.pointer = 0;
       }
+      handleTimeout(nextState);
       return nextState;
     } break;
     case 'STOP': {
+      clearTimeout(nextState.timeout);
       nextState.isPlaying = false;
+      handleTimeout(nextState);
       return nextState;
     } break;
     case 'PLAY': {
+      clearTimeout(nextState.timeout);
       nextState.isPlaying = true;
+      handleTimeout(nextState);
       return nextState;
     } break;
     case 'PREVIOUS': {
+      clearTimeout(nextState.timeout);
       if (--nextState.pointer < 0) {
         nextState.pointer = nextState.collection.length - 1;
       }
+      handleTimeout(nextState);
       return nextState;
     } break;
     default: {
       /* Return original state if no actions were consumed. */
       return state;
     }
+  }
+}
+
+function handleTimeout(state) {
+  clearTimeout(state.timeout);
+  if (state.isPlaying) {
+    console.log('should dispatch play after timeout');
+  }
+  else {
+    console.log('should do nothing');
   }
 }
