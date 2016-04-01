45e69ed05905018c9234bddd429299fd842890c1
Thomas Kolar <thomas.kolar@email.de>
2016-04-01 09:37:32 +0200

    Fixes an unreachable code warning

--- src/reducers/slides.js
+++ src/reducers/slides.js
@@ -25,24 +25,24 @@ module.exports = function(state = initialState, action) {
         nextState.pointer = 0;
       }
       return nextState;
-    } break;
+    }
     case 'STOP': {
       clearTimeout(nextState.timeout);
       nextState.isPlaying = false;
       return nextState;
-    } break;
+    }
     case 'PLAY': {
       clearTimeout(nextState.timeout);
       nextState.isPlaying = true;
       return nextState;
-    } break;
+    }
     case 'PREVIOUS': {
       clearTimeout(nextState.timeout);
       if (--nextState.pointer < 0) {
         nextState.pointer = nextState.collection.length - 1;
       }
       return nextState;
-    } break;
+    }
     default: {
       /* Return original state if no actions were consumed. */
       return state;
