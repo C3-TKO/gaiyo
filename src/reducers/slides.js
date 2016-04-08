c7d1a43327d23739923ddecfb81fcea8055afce8
Thomas Kolar <thomas.kolar@email.de>
2016-04-08 17:30:05 +0200

    Cleans up initial state of slides reducer

--- src/reducers/slides.js
+++ src/reducers/slides.js
@@ -3,14 +3,7 @@
  * If you change the type from object to something else, do not forget to update
  * src/container/App.js accordingly.
  */
-const initialState = {
-  /*'collection': [
-    {'_id': '1', '_rev' : '', 'sortOrder': 1, 'url': 'screen_1.html', 'timeout': 5000},
-    {'_id': '2', '_rev' : '', 'sortOrder': 2, 'url': 'screen_2.html', 'timeout': 5000},
-    {'_id': '3', '_rev' : '', 'sortOrder': 3, 'url': 'screen_3.html', 'timeout': 5000}
-  ],*/
-  'collection': []
-};
+const initialState = {};
 
 module.exports = function(state = initialState, action) {
   /* Keep the reducer clean - do not mutate the original state. */
