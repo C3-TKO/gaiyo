fe0a5368356d647395b812161b9e4f0c858dbefb
Thomas Kolar <thomas.kolar@email.de>
2016-04-08 17:30:46 +0200

    Adds a reducer for the state of the controls

---
+++ src/reducers/control.js
@@ -0,0 +1,45 @@
+/* Define your initial state here.
+ *
+ * If you change the type from object to something else, do not forget to update
+ * src/container/App.js accordingly.
+ */
+const initialState = {
+  'timeout' : undefined,
+  'isPlaying' : false,
+  'pointer': 0
+};
+
+module.exports = function(state = initialState, action) {
+  /* Keep the reducer clean - do not mutate the original state. */
+  //let nextState = Object.assign({}, state);
+
+  switch(action.type) {
+    case 'NEXT':
+      if (++nextState.pointer >= nextState.collection.length) {
+        nextState.pointer = 0;
+      }
+      return nextState;
+
+    case 'STOP':
+      clearTimeout(nextState.timeout);
+      nextState.isPlaying = false;
+      return nextState;
+
+    case 'PLAY':
+      clearTimeout(nextState.timeout);
+      nextState.timeout = action.timeout;
+      nextState.isPlaying = true;
+      return nextState;
+
+    case 'PREVIOUS':
+      if (--nextState.pointer < 0) {
+        nextState.pointer = nextState.collection.length - 1;
+      }
+      return nextState;
+
+    default: {
+      /* Return original state if no actions were consumed. */
+      return state;
+    }
+  }
+}
