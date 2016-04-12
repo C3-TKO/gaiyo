155b83ff8e3928cd19f0d74fdfe6426a54089132
Thomas Kolar <thomas.kolar@email.de>
2016-04-12 23:01:07 +0200

    Adds an edit mode to the slide list item component

--- src/components/SlideListItemComponent.js
+++ src/components/SlideListItemComponent.js
@@ -4,16 +4,67 @@ import React from 'react';
 import TableRow from 'material-ui/lib/table/table-row';
 import TableRowColumn from 'material-ui/lib/table/table-row-column';
 import ActionDelete from 'material-ui/lib/svg-icons/action/delete';
+import TextField from 'material-ui/lib/text-field';
+import EditorModeEdit from 'material-ui/lib/svg-icons/editor/mode-edit';
 
 require('styles//SlideListItem.scss');
 
 class SlideListItemComponent extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false
+    };
+  }
+
   removeSlideFromList = () => {
     this.props.onDelete(this.props.slide._id);
   }
 
+  handleEditSlide = () => {
+    if(this.state.editing) {
+      this.setState({
+        editing: false
+      })
+    }
+    else {
+      this.setState({
+        editing: true
+      })
+    }
+  }
+
   render() {
+    if(this.state.editing) {
+      return(
+        <TableRow>
+          <TableRowColumn>
+            <TextField
+              ref="addSlideUrl"
+              floatingLabelText="Url"
+              defaultValue={this.props.slide.url}
+            />
+            </TableRowColumn>
+          <TableRowColumn>
+            <TextField
+              ref="addSlideDuration"
+              floatingLabelText="Duration (ms)"
+              defaultValue={this.props.slide.duration}
+            />
+          </TableRowColumn>
+          <TableRowColumn>
+            <ActionDelete
+              onTouchTap={this.removeSlideFromList}
+            />
+            <EditorModeEdit
+              onTouchTap={this.handleEditSlide}
+            />
+          </TableRowColumn>
+        </TableRow>
+      )
+    }
+
     return (
       <TableRow>
         <TableRowColumn>{this.props.slide.url}</TableRowColumn>
@@ -21,6 +72,9 @@ class SlideListItemComponent extends React.Component {
         <TableRowColumn>
           <ActionDelete
             onTouchTap={this.removeSlideFromList}
+          />
+          <EditorModeEdit
+            onTouchTap={this.handleEditSlide}
           />
         </TableRowColumn>
       </TableRow>
