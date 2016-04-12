d252432033d28939ad9e1ddde287d972ec117980
Thomas Kolar <thomas.kolar@email.de>
2016-04-12 08:34:23 +0200

    Removes unneccesary component imports

--- src/components/SlideListComponent.js
+++ src/components/SlideListComponent.js
@@ -5,9 +5,7 @@ import Table from 'material-ui/lib/table/table';
 import TableHeaderColumn from 'material-ui/lib/table/table-header-column';
 import TableRow from 'material-ui/lib/table/table-row';
 import TableHeader from 'material-ui/lib/table/table-header';
-import TableRowColumn from 'material-ui/lib/table/table-row-column';
 import TableBody from 'material-ui/lib/table/table-body';
-import RaisedButton from 'material-ui/lib/raised-button';
 import SlideListItem from './SlideListItemComponent';
 
 require('styles//SlideList.scss');
