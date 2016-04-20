9e5c2e077bdc12e0ae663cf3a43598106ae04084
Thomas Kolar <thomas.kolar@email.de>
2016-04-20 22:35:22 +0200

    Adds the components for the icon menu

--- src/components/SlideListComponent.js
+++ src/components/SlideListComponent.js
@@ -5,6 +5,28 @@ import SlideListItem from './SlideListItemComponent';
 import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
 
+import IconButton from 'material-ui/lib/icon-button';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+
+const iconButtonElement = (
+  <IconButton
+    touch={true}
+    tooltip="more"
+    tooltipPosition="bottom-left"
+  >
+    <MoreVertIcon/>
+  </IconButton>
+);
+
+const rightIconMenu = (
+  <IconMenu iconButtonElement={iconButtonElement}>
+    <MenuItem>Edit</MenuItem>
+    <MenuItem>Delete</MenuItem>
+  </IconMenu>
+);
+
 require('styles//SlideList.scss');
 
 class SlideListComponent extends React.Component {
@@ -21,6 +43,7 @@ class SlideListComponent extends React.Component {
               onUpdate={this.props.onUpdate}
               primaryText={slide.url}
               secondaryText={slide.duration}
+              rightIconButton={rightIconMenu}
             />
           )}
         </List>
