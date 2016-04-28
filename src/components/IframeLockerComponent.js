836e20afde7f595c139fabc9c9ebebedab5238b0
Thomas Kolar <thomas.kolar@email.de>
2016-04-28 16:22:26 +0200

    Adds a shim above the iframe content in order to prevent user interaction

---
+++ src/components/IframeLockerComponent.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//IframeLocker.scss');
+
+class IframeLockerComponent extends React.Component {
+  render() {
+    return (
+      <div
+        className="iframelocker-component"
+      />
+    );
+  }
+}
+
+IframeLockerComponent.displayName = 'IframeLockerComponent';
+
+
+export default IframeLockerComponent;
