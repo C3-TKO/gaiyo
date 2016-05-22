c40a2dbcdd89946fd8d1a00608ecbe34fd27b4fa
Thomas Kolar <thomas.kolar@email.de>
2016-05-22 23:09:25 +0200

    Adds first running formsy element

--- src/components/EditSlideFormComponent.js
+++ src/components/EditSlideFormComponent.js
@@ -2,11 +2,20 @@
 
 import React from 'react';
 import TextField from 'material-ui/TextField';
+import Formsy from 'formsy-react';
+import {FormsyText} from 'formsy-material-ui/lib';
 
 require('styles//EditSlideForm.scss');
 
 class EditSlideFormComponent extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      canSubmit: false
+    };
+  }
+  
   handleSave = () => {
     const slide = {
       'url': this.refs.url.getValue(),
@@ -21,9 +30,45 @@ class EditSlideFormComponent extends React.Component {
     }
   }
 
+  enableButton = () => {
+    this.setState({
+      canSubmit: true
+    });
+  }
+
+  disableButton = () => {
+    this.setState({
+      canSubmit: false
+    });
+  }
+
+  submitForm = (data) =>  {
+    alert(JSON.stringify(data, null, 4));
+  }
+
+  notifyFormError(data) {
+    console.error('Form error:', data);
+  }
+
   render() {
     return (
       <div className="editslideform-component">
+        <Formsy.Form
+          onValid={this.enableButton}
+          onInvalid={this.disableButton}
+          onValidSubmit={this.submitForm}
+          onInvalidSubmit={this.notifyFormError}
+        >
+          <FormsyText
+            name="url"
+            validations="isUrl"
+            validationError='URL IS WRONG!'
+            required
+            hintText="http://www.example.com"
+            floatingLabelText="URL"
+          />
+        </Formsy.Form>
+
         <TextField
           fullWidth={true}
           ref="url"
