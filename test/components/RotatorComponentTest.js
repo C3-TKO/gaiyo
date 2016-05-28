0485d09ba334b4a631a891c69d381a0faf68faeb
Thomas Kolar <thomas.kolar@email.de>
2016-05-28 23:29:59 +0200

    Adds the spec for rotator component

--- test/components/RotatorComponentTest.js
+++ test/components/RotatorComponentTest.js
@@ -26,4 +26,40 @@ describe('RotatorComponent', () => {
   it('should have its component name as default className', () => {
     expect(component.prop('className')).to.equal('rotator-component');
   });
+
+  it('should reset the slide pointer if it is out bounds after deletion of a slide', () => {
+
+  });
+
+  describe('Screen rotator controls', () => {
+    it('should start the timeouted display of the current screen on play()', () => {
+
+    });
+
+    it('should pause the screen rotation on stop()', () => {
+
+    });
+
+    it('should show the following screen in list on next()', () => {
+
+    });
+
+    it('should show the previous screen in list on prev()', () => {
+
+    });
+
+    it('should go to a specific screen on goto() call using the slide id', () => {
+
+    });
+
+    describe('Inifinite looping', () => {
+      it('should start over with the first screen in list after the last one has been displayed', () => {
+
+      });
+
+      it('should load last screen of list if using prev() while displaying the very first screen', () => {
+
+      });
+    });
+  });
 });
