b9fe4a6288cddbfa873f2a0fcbce9ffae4950d79
Thomas Kolar <thomas.kolar@email.de>
2016-05-30 22:47:10 +0200

    Adds the react-intl npm package / implemnts the provider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './stores';
+import {IntlProvider} from 'react-intl';
 import App from './containers/App';
 
 const store = configureStore();
 
 render(
   <Provider store={store}>
-    <App />
+    <IntlProvider locale='en'>
+      <App />
+    </IntlProvider>
   </Provider>,
   document.getElementById('app')
 );
