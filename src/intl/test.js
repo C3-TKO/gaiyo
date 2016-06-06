ce0d8cd6865e3a006f28a1f41264b2df07c1e899
Thomas Kolar <thomas.kolar@email.de>
2016-06-06 22:53:47 +0200

    Adds the working prototype for message definition in separate files

---
+++ src/intl/test.js
@@ -0,0 +1,33 @@
+// jscs:disable disallowQuotedKeysInObjects
+export default {
+  locales: ['en'],
+
+  messages: {
+    'settingsClose': 'Close',
+    'settingsTitle': 'Settings',
+    'settingsSnackbar': 'Please add at least one screen to the rotation list!',
+
+    'screenlauncherSubHeader': 'Click to select',
+
+    'editslideformLabelUrl': 'Url (required)',
+    'editslideformErrorUrl': 'Please enter a valid url',
+    'editslideformLabelDuration': 'Duration in seconds (required)',
+    'editslideformErrorDuration': 'Please enter a valid duration',
+
+    'screenlauncherTitle': 'Go to screen',
+    'screenlauncherClose': 'Close',
+
+    'editslideformTitle': 'Edit slide',
+    'editslideformSubHeader': 'Screen rotation list',
+    'editslideformSecondaryText': `{duration, plural,
+          one {{duration} second}
+          other {{duration} seconds}}`,
+    'editslideformButtonUpdate': 'Update',
+    'editslideformButtonCancel': 'Cancel',
+    'editslideformButtonDelete': 'Delete',
+    'editslideformButtonEdit': 'Edit',
+    'editslideformButtonMore': 'more',
+
+    'iframelockerSnackbarMessage': 'This app runs in read only mode! You cannot interact with the websites that are shown!'
+  }
+};
