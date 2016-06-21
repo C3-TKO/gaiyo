b3c272162d8f3dfbd028b3b7ae3dc3637f4b94cc
Thomas Kolar <thomas.kolar@email.de>
2016-06-21 20:05:21 +0200

    Ensures pure state handling

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -6,11 +6,11 @@ const initialState = {
 
 module.exports = function(state = initialState, action) {
   /* Keep the reducer clean - do not mutate the original state. */
-  //let nextState = Object.assign({}, state);
+  let nextState = Object.assign({}, state);
 
   switch(action.type) {
     case 'EDIT_SETTINGS': {
-      return action.settings;
+      return nextState.settings = action.settings;
     }
     default: {
       /* Return original state if no actions were consumed. */
