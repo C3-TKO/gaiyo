c891842b1275ff6782ca6849d743883e389d5931
Thomas Kolar <thomas.kolar@email.de>
2016-06-27 22:06:22 +0200

    Adds timebased revision of sync settings

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -1,13 +1,31 @@
+const date = new Date();
+
 const initialState = {
   remoteDbUrl: undefined,
   syncMode: 1,
-  enabled: false
+  enabled: false,
+  lastChanged: date.getTime()
 };
 
 module.exports = function(state = initialState, action) {
   switch(action.type) {
     case 'EDIT_SETTINGS': {
-      return action.settings;
+      console.log(state, action.settings);
+      let nextState = action.settings;
+      if(
+        action.settings.remoteDbUrl === state.remoteDbUrl &&
+        action.settings.syncMode === state.syncMode &&
+        action.settings.enabled === state.enabled
+      ) {
+        Object.assign(nextState, {lastChanged: state.lastChanged});
+      }
+      else {
+        const changeDate = new Date();
+        Object.assign(nextState, {lastChanged: changeDate.getTime()});
+      }
+      console.log(nextState);
+
+      return nextState;
     }
     default: {
       /* Return original state if no actions were consumed. */
