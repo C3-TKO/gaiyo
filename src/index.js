3ff16d1b10ff9b6dd42c96512e0ce9452af3be0a
Thomas Kolar <thomas.kolar@email.de>
2016-07-08 22:57:40 +0200

    Moves th muitheme provider to index.js - now all providers are located in the same place

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import configureStore from './stores';
 import { IntlProvider } from 'react-intl';
 import App from './containers/App';
 import messages from './intl/en';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const store = configureStore();
 
 render(
   <Provider store={store}>
     <IntlProvider locale='en' messages={messages}>
-      <App />
+      <MuiThemeProvider muiTheme={getMuiTheme()} className="index">
+        <App />
+      </MuiThemeProvider>
     </IntlProvider>
   </Provider>,
   document.getElementById('app')
