8ff6710c14f93fd26d94ad8eda964446c9fbb16c
Tobias Wild <tobiwild@web.de>
2016-07-10 18:22:27 +0200

    add fade in/out transitions for iframe

--- src/components/IframeComponent.js
+++ src/components/IframeComponent.js
@@ -6,16 +6,28 @@ require('styles//Iframe.scss');
 
 class IframeComponent extends React.Component {
   render() {
-    return (
-      <iframe className="iframe-component" src={this.props.url}></iframe>
-    );
+    if (this.props.urlOut === this.props.urlIn) {
+      return (
+        <div>
+          <iframe key={this.props.urlIn} className="iframe-component" src={this.props.urlIn}></iframe>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <iframe key={this.props.urlOut} className="iframe-component fade-out" src={this.props.urlOut}></iframe>
+          <iframe key={this.props.urlIn} className="iframe-component fade-in" src={this.props.urlIn}></iframe>
+        </div>
+      );
+    }
   }
 }
 
 IframeComponent.displayName = 'IframeComponent';
 
 IframeComponent.propTypes = {
-  url: React.PropTypes.string.isRequired
+  urlOut: React.PropTypes.string.isRequired,
+  urlIn: React.PropTypes.string.isRequired
 };
 
 export default IframeComponent;
--- test/components/IframeComponentTest.js
+++ test/components/IframeComponentTest.js
@@ -12,15 +12,44 @@ import IframeComponent from 'components//IframeComponent.js';
 describe('IframeComponent', () => {
   let component;
 
-  beforeEach(() => {
-    component = createComponent(IframeComponent, Object.assign({},
-      {
-        url: 'http://www.example.com'
-      })
-    )
+  describe('with urlOut equals urlIn', () => {
+    beforeEach(() => {
+      component = createComponent(IframeComponent, Object.assign({},
+        {
+          urlOut: 'http://www.example.com',
+          urlIn: 'http://www.example.com'
+        })
+      )
+    });
+
+    it('should have single iframe', () => {
+      expect(component.props.children.props.className).to.equal('iframe-component')
+    });
   });
 
-  it('should have its component name as default className', () => {
-    expect(component.props.className).to.equal('iframe-component');
+  describe('with urlOut not equals urlIn', () => {
+    beforeEach(() => {
+      component = createComponent(IframeComponent, Object.assign({},
+        {
+          urlOut: 'http://www.example.com',
+          urlIn: 'http://www.example2.com'
+        })
+      )
+    });
+
+    it('should have two iframes', () => {
+      expect(component.props.children.length).to.equal(2)
+    });
+
+    it('should have first iframe with fade-out class', () => {
+      const iframe = component.props.children[0]
+      expect(iframe.props.className).to.equal('iframe-component fade-out')
+    });
+
+    it('should have second iframe with fade-in class', () => {
+      const iframe = component.props.children[1]
+      expect(iframe.props.className).to.equal('iframe-component fade-in')
+    });
   });
+
 });
