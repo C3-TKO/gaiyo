0a18882a05cccc1e5571229553b65465496d4ab7
Thomas Kolar <thomas.kolar@email.de>
2016-07-31 15:04:33 +0200

    Adds an onLoad event trigger for the iframe
    - Checks if the content of the iframe could be loaded
    - Checks the accessibility of the iframe content as well
    - Prepares the check for the non loading X-FRAME options timeout

--- src/components/IframeComponent.js
+++ src/components/IframeComponent.js
@@ -13,18 +13,43 @@ class IframeComponent extends React.Component {
     }
   }
 
+  onload = () => {
+    // This method will only be invoked if the iframe trigger the onLoad event
+    const iframe = document.getElementsByClassName('iframe-component')[0];
+    try {
+      const checkAaccess = (iframe.contentWindow || iframe.contentDocument).location.href;
+    } catch (error) {
+      // This indicates that the iframe could have been loaded but not accessed due to sandbox restrictions.
+    }
+  }
+
   render() {
     if (this.props.urlOut === this.props.urlIn) {
       return (
         <div>
-          <iframe key={this.props.urlIn} className="iframe-component" src={this.props.urlIn}></iframe>
+          <iframe
+            key={this.props.urlIn}
+            className="iframe-component"
+            src={this.props.urlIn}
+          />
         </div>
       );
     } else {
       return (
         <div>
-          <iframe key={this.props.urlOut} style={this.getStyle('out')} className="iframe-component" src={this.props.urlOut}></iframe>
-          <iframe key={this.props.urlIn} style={this.getStyle('in')} className="iframe-component" src={this.props.urlIn}></iframe>
+          <iframe
+            key={this.props.urlOut}
+            style={this.getStyle('out')}
+            className="iframe-component"
+            src={this.props.urlOut}
+          />
+          <iframe
+            key={this.props.urlIn}
+            style={this.getStyle('in')}
+            className="iframe-component"
+            src={this.props.urlIn}
+            onLoad={this.onload()}
+          />
         </div>
       );
     }
