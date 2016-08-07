1ecf0539179667aa13bdf5ba81933ff7dcba385b
Thomas Kolar <thomas.kolar@email.de>
2016-08-07 21:39:07 +0200

    Adds the initial state spec for settings reducer

--- test/reducers/settingsTest.js
+++ test/reducers/settingsTest.js
@@ -1,6 +1,20 @@
 var reducer = require('../../src/reducers/settings');
 
-describe('settings', () => {
+describe.only('settings reducer', () => {
+  it('should return the initial state', () => {
+    expect(
+      reducer(undefined, {})
+    ).to.deep.equal(
+      {
+        remoteDbUrl: undefined,
+        remoteDbUser: undefined,
+        remoteDbPassword: undefined,
+        syncMode: 1,
+        enabled: false,
+        lastChanged: undefined
+      }
+    )
+  })
 
   it('should not change the passed state', (done) => {
 
@@ -9,4 +23,5 @@ describe('settings', () => {
 
     done();
   });
+  
 });
