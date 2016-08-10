8a3df91506abe74b94248e8eab6001b639bc24bb
Thomas Kolar <thomas.kolar@email.de>
2016-08-10 15:03:26 +0200

    Fixes wrongly merged pull request

---
+++ test/actions/ActionsTest.js
@@ -0,0 +1,58 @@
+import addSlideAction from 'actions//addSlide.js'
+import deleteSlideAction from 'actions//deleteSlide.js'
+import editSlideAction from 'actions//editSlide.js'
+import editSettingsAction from 'actions//editSettings.js'
+
+describe('Actions', () => {
+  it('should create an action to add a slide', () => {
+    const slide = {
+      url: 'http://www.example.com',
+      duration: 5000,
+      id: 'test'
+    }
+    const expectedAction = {
+      type: 'ADD_SLIDE',
+      slide
+    }
+    expect(addSlideAction(slide)).to.deep.equal(expectedAction)
+  })
+
+  it('should create an action to delete a slide', () => {
+    const idSlideToDelete = 'someid';
+    const expectedAction = {
+      type: 'DELETE_SLIDE',
+      id: idSlideToDelete
+    }
+    expect(deleteSlideAction(idSlideToDelete)).to.deep.equal(expectedAction)
+  })
+
+  it('should create an action to edit an exsiting slide', () => {
+    const slideId = 'someId';
+    const slide = {
+      url: 'http://www.example.com',
+      duration: 5000,
+      id: slideId
+    }
+    const expectedAction = {
+      type: 'EDIT_SLIDE',
+      id: slideId,
+      slide: slide
+    }
+    expect(editSlideAction(slideId, slide)).to.deep.equal(expectedAction)
+  })
+
+  it('should create an action to edit settings', () => {
+
+    const settings = {
+      remoteDbUrl: 'http://www.exampe.com:5984',
+      remoteDbUser: 'foo',
+      remoteDbPassword: 'bar',
+      syncMode: '3'
+    }
+    const expectedAction = {
+      type: 'EDIT_SETTINGS',
+      settings: settings
+    }
+    expect(editSettingsAction(settings)).to.deep.equal(expectedAction)
+  })
+})
