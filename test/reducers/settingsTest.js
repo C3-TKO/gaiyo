3fccde566c912f0c9e961b8b3596725f2874ef1e
Thomas Kolar <thomas.kolar@email.de>
2016-08-12 12:24:47 +0200

    Refactors the settings reducer test

--- test/reducers/settingsTest.js
+++ test/reducers/settingsTest.js
@@ -1,4 +1,5 @@
 var reducer = require('../../src/reducers/settings');
+import editSettingsAction from '../../src/actions/editSettings.js';
 
 const initialStateRaw = {
   remoteDbUrl: undefined,
@@ -27,35 +28,25 @@ describe('settings reducer', () => {
 
   it('should handle EDIT_SETTINGS when there is no change', () => {
     expect(
-      reducer(initialStateSpecification,
-        {
-          type: 'EDIT_SETTINGS',
-          settings: {
-            remoteDbUrl: undefined,
-            remoteDbUser: undefined,
-            remoteDbPassword: undefined,
-            syncMode: 1,
-            enabled: false
-          }
-        }
-      )
+      reducer(initialStateSpecification, editSettingsAction({
+        remoteDbUrl: undefined,
+        remoteDbUser: undefined,
+        remoteDbPassword: undefined,
+        syncMode: 1,
+        enabled: false
+      }))
     ).to.deep.equal(initialStateSpecification)
   })
 
   it('should handle EDIT_SETTINGS when there is a change', () => {
     expect(
-      reducer(initialStateSpecification,
-        {
-          type: 'EDIT_SETTINGS',
-          settings: {
-            remoteDbUrl: 'http://example.com',
-            remoteDbUser: 'test-user',
-            remoteDbPassword: 'test-pwd',
-            syncMode: 3,
-            enabled: true
-          }
-        }
-      )
+      reducer(initialStateSpecification, editSettingsAction({
+        remoteDbUrl: 'http://example.com',
+        remoteDbUser: 'test-user',
+        remoteDbPassword: 'test-pwd',
+        syncMode: 3,
+        enabled: true
+      }))
     ).to.deep.equal(
       {
         remoteDbUrl: 'http://example.com',
