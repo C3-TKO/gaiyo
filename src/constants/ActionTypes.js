14285f1a7e5701e4b490cc6a00cda4b3b41df9ce
Thomas Kolar <thomas.kolar@email.de>
2016-08-12 11:22:21 +0200

    Adds the action types used by the pouchdb middleware plugin

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -3,3 +3,7 @@ export const EDIT_SLIDE = 'EDIT_SLIDE'
 export const DELETE_SLIDE = 'DELETE_SLIDE'
 export const EDIT_SETTINGS = 'EDIT_SETTINGS'
 export const UPDATE_SYNC_STATE = 'UPDATE_SYNC_STATE'
+
+// Used for pouchdb-middleware plugin
+export const INSERT_SLIDE = 'INSERT_SLIDE'
+export const UPDATE_SLIDE = 'UPDATE_SLIDE'
