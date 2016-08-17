9d725e2a08e7a61613a8ce24874334ccfad0a3cd
Thomas Kolar <thomas.kolar@email.de>
2016-08-17 21:02:36 +0200

    Adds a basic rendering unit test for the reloader component

--- test/components/ReloaderComponentTest.js
+++ test/components/ReloaderComponentTest.js
@@ -3,20 +3,27 @@
 /*eslint no-console: 0*/
 'use strict';
 
-// Uncomment the following lines to use the react test utilities
-// import TestUtils from 'react-addons-test-utils';
-import createComponent from 'helpers/shallowRenderHelper';
+import React from 'react';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { mountWithIntl, shallowWithIntl } from 'helpers/intl-enzyme-test-helper.js';
 
 import ReloaderComponent from 'components//ReloaderComponent.js';
 
-describe.skip('ReloaderComponent', () => {
-  let component;
+describe('ExportSlidesComponent', () => {
+
+  let wrapper;
 
   beforeEach(() => {
-    component = createComponent(ReloaderComponent);
+    wrapper = mountWithIntl(
+      <ReloaderComponent
+        pointer={1}
+        durationLastScreen={5000}
+        indexOfLastSlide={5}
+      />
+    );
   });
 
-  it('should have its component name as default className', () => {
-    expect(component.props.className).to.equal('reloader-component');
+  it('should have its component name as default className for the containing div', () => {
+    expect(wrapper.find('div.reloader-component')).to.have.length(1);
   });
 });
