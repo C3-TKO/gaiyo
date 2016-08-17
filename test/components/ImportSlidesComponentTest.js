24d793b33002e8ff8602b9644142e36f710dd0f1
Thomas Kolar <thomas.kolar@email.de>
2016-08-17 21:30:01 +0200

    Adds a basic rendering unit test for the import slides component

--- test/components/ImportSlidesComponentTest.js
+++ test/components/ImportSlidesComponentTest.js
@@ -3,20 +3,34 @@
 /*eslint no-console: 0*/
 'use strict';
 
-// Uncomment the following lines to use the react test utilities
-// import TestUtils from 'react-addons-test-utils';
-import createComponent from 'helpers/shallowRenderHelper';
-
+import React from 'react';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { mountWithIntl, shallowWithIntl } from 'helpers/intl-enzyme-test-helper.js';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 import ImportSlidesComponent from 'components//ImportSlidesComponent.js';
 
-describe.skip('ImportSlidesComponent', () => {
-  let component;
-
-  beforeEach(() => {
-    component = createComponent(ImportSlidesComponent);
+describe('ImportSlidesComponent', () => {
+  let wrapper;
+  const mockStore = configureStore();
+  const store = mockStore({
+    slides: []
   });
 
-  it('should have its component name as default className', () => {
-    expect(component.props.className).to.equal('importslides-component');
+  beforeEach(() => {
+    wrapper = mountWithIntl(
+      <Provider store={store}>
+        <ImportSlidesComponent
+          slides={[]}
+          goto={() => {}}
+          onMouseEnter={() => {}}
+          onMouseLeave={() => {}}
+        />
+      </Provider>
+    );
+  });
+
+  it('should have its component name as default className for the containing div', () => {
+    expect(wrapper.find('div.importslides-component')).to.have.length(1);
   });
 });
